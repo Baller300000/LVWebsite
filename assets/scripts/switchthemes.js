1dda63a4e1c2f721defeb8faa9622334950624c4
Baller300000 <lvyssoulis@outlook.com>
2025-12-27 13:47:59 +0200

    fix background sizing

--- assets/scripts/switchthemes.js
+++ assets/scripts/switchthemes.js
@@ -4,11 +4,13 @@ btn.onclick = () => {
     night = !night;
     if (night) {
         document.body.style.background = "url('assets/images/night-sky.avif') repeat center center fixed";
-        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundRepeat = "repeat";
+        document.body.style.backgroundSize = "auto";
         btn.textContent = "Switch Theme";
     } else {
         document.body.style.background = "url('assets/images/sky.avif') repeat center center fixed";
-        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundRepeat = "repeat";
+        document.body.style.backgroundSize = "auto";
         btn.textContent = "Switch Theme";
     }
 };
