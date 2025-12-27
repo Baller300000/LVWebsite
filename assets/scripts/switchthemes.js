56be8d7483bf889b86948f4fd8a0023ead39d723
Baller300000 <lvyssoulis@outlook.com>
2025-12-27 13:44:55 +0200

    add better night sky background

--- assets/scripts/switchthemes.js
+++ assets/scripts/switchthemes.js
@@ -3,11 +3,11 @@ let night = false;
 btn.onclick = () => {
     night = !night;
     if (night) {
-        document.body.style.background = "url('assets/images/night-sky.avif') no-repeat center center fixed";
+        document.body.style.background = "url('assets/images/night-sky.avif') repeat center center fixed";
         document.body.style.backgroundSize = "cover";
         btn.textContent = "Switch Theme";
     } else {
-        document.body.style.background = "url('assets/images/sky.avif') no-repeat center center fixed";
+        document.body.style.background = "url('assets/images/sky.avif') repeat center center fixed";
         document.body.style.backgroundSize = "cover";
         btn.textContent = "Switch Theme";
     }
