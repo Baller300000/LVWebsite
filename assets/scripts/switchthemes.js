ab0cd9672b1a0e750d087db93de1d45be2f7be45
Baller300000 <lvyssoulis@outlook.com>
2026-01-03 18:42:12 +0200

    fix background script

--- assets/scripts/switchthemes.js
+++ assets/scripts/switchthemes.js
@@ -8,7 +8,7 @@ btn.onclick = () => {
         document.body.style.backgroundSize = "auto";
         btn.textContent = "Switch Theme";
     } else {
-        document.body.style.background = "rgb(219, 169, 77) repeat center center fixed";
+        document.body.style.background = "rgb(182, 127, 26) repeat center center fixed";
         document.body.style.backgroundRepeat = "repeat";
         document.body.style.backgroundSize = "auto";
         btn.textContent = "Switch Theme";
