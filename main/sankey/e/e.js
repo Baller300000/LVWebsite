bb69a15a6912aa250dec29dd58cb56ee7d0d1807
Baller300000 <lvyssoulis@outlook.com>
2026-03-30 09:35:03 +0300

    add statistics to ajsdjkalsdjl

--- main/sankey/e/e.js
+++ main/sankey/e/e.js
@@ -3,3 +3,7 @@ var t = setInterval(function () {
     var ele = document.getElementById('aich');
     ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
 }, blink_speed);
+var t = setInterval(function () {
+    var ele2 = document.getElementById('aich2');
+    ele2.style.visibility = (ele2.style.visibility == 'hidden' ? '' : 'hidden');
+}, blink_speed);
